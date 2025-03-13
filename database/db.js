360e9295415c8954dc0f735bc58744761d6d8688
Diego Damasceno <diego.d.dl@outlook.com>
2025-03-12 21:11:20 -0300

    aplicacao do env

--- database/db.js
+++ database/db.js
@@ -4,8 +4,7 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const connectToDB = () => {
-    // const mongoUri = process.env.MONGO_URI;
-    const mongoUri = 'mongodb+srv://diegod1199:8NdpDARYNfzt5cDg@todolist.niw7n.mongodb.net/?retryWrites=true&w=majority&appName=todolist';
+    const mongoUri = process.env.MONGO_URI;
     if (!mongoUri) {
         console.error("Erro: MONGO_URI não está definida ou está vazia!");
         return;
